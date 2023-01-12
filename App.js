1a34b018e57685a4da019cfeb1c058ee7b80d6ee
tain <phattai2407@gmail.com>
2023-01-12 09:58:34 +0200

    added navigation

--- App.js
+++ App.js
@@ -1,33 +1,14 @@
-/* eslint-disable react/jsx-no-undef */
-import {
-  FlatList,
-  StyleSheet,
-  TouchableOpacity,
-  View,
-  Image,
-  Text,
-  SafeAreaView,
-  Platform,
-} from 'react-native';
 import {StatusBar} from 'expo-status-bar';
-import List from './components/List';
+import Navigator from './navigators/Navigators';
 
 
 const App = () => {
   return (
-    <SafeAreaView style={styles.container}>
-      <List />
+    <>
+      <Navigator></Navigator>
       <StatusBar style="auto" />
-    </SafeAreaView>
+    </>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor:'#fff',
-    paddingTop: Platform.OS === 'android' ? 30 : 0,
-  },
-});
 
 export default App;
---
+++ navigators/Navigators.js
@@ -0,0 +1,20 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import React from "react";
+import Home from "../views/Home";
+import Profile from "../views/Profile";
+
+const Tab = createBottomTabNavigator();
+
+const Navigator = () => {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Profile" component={Profile} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  )
+};
+
+export default Navigator;
