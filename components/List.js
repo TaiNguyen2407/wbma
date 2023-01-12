e21382e781b92d6c50db0953d1d1b4d44fbeb887
tain <phattai2407@gmail.com>
2023-01-12 11:19:28 +0200

    added latest updates

--- components/List.js
+++ components/List.js
@@ -1,9 +1,10 @@
 import {FlatList, StyleSheet} from 'react-native';
 import { useMedia } from '../hooks/ApiHooks';
 import ListItem from './ListItem';
+import PropTypes from 'prop-types';
 
 
-const List = () => {
+const List = ({navigation}) => {
   const {mediaArray} = useMedia();
   return (
     <FlatList style={styles.flatList}
@@ -11,6 +12,7 @@ const List = () => {
       keyExtractor={(item, index) => index.toString()}
       renderItem={({item}) =>
       <ListItem
+      navigation={navigation}
       singleMedia={item}/>}
     />
   )
@@ -19,6 +21,10 @@ const List = () => {
 const styles = StyleSheet.create({
   flatList: {
   }
-})
+});
+
+List.propTypes = {
+  navigation: PropTypes.object,
+};
 
 export default List;
