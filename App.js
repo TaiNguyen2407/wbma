70a6bee696624e03038ac1226b1ec718102ab12e
tain <phattai2407@gmail.com>
2023-01-16 11:02:36 +0200

    merged dummy-login to main

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StatusBar } from 'react-native';
+// import { StatusBar } from 'react-native';
 import BackgroundImage from './components/BackgroundImage';
 import {StatusBar} from 'expo-status-bar';
 import { MainProvider } from './contexts/MainContext';
@@ -10,7 +10,7 @@ const App = () => {
     <MainProvider>
       <Navigator></Navigator>
       <StatusBar style="auto" />
-      <BackgroundImage />
+      {/* <BackgroundImage /> */}
     </MainProvider>
   );
 };
