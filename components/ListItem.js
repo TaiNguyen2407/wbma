cd8e16dd03c9001dcb4db590eb1265211c056189
tain <phattai2407@gmail.com>
2023-02-08 12:06:43 +0200

    implementing unlocking view

--- components/ListItem.js
+++ components/ListItem.js
@@ -1,11 +1,13 @@
-import React, {useState} from 'react';
+import React, {useContext, useState} from 'react';
 // import { StyleSheet} from 'react-native';
 import { uploadsUrl } from '../utils/variables';
 import { useNavigation } from '@react-navigation/native';
 import PropTypes from 'prop-types';
-import { Avatar, Button, Card, Divider, ListItem as RNEListItem } from '@rneui/themed';
+import { Avatar, Button, ButtonGroup, Card, Divider, ListItem as RNEListItem } from '@rneui/themed';
+import { MainContext } from '../contexts/MainContext';
 
 const ListItem = ({singleMedia}) => {
+  const {user} = useContext(MainContext);
   const item = singleMedia;
   const navigation = useNavigation();
   return (
@@ -15,6 +17,19 @@ const ListItem = ({singleMedia}) => {
         <RNEListItem.Title >{item.title}</RNEListItem.Title>
         <RNEListItem.Subtitle numberOfLines={1}>{item.description}</RNEListItem.Subtitle>
       </RNEListItem.Content>
+      {item.user_id === user.user_id && (
+        <ButtonGroup
+        button={['Modify', 'Delete']}
+        rounded
+        onPress={(index) => {
+          if (index === 0) {
+            console.log('Modify pressed')
+          }else {
+            console.log('Delete pressed')
+          }
+        }} />
+      )}
+
       <Button onPress={() => {
         navigation.navigate('Single', item);
       }}>View</Button>
